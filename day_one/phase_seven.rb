0497d05d1cc9bf37e41fd1097d7d519f9baa9cd9
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2023-07-01 17:08:52 +0700

    Explanation on phase 7

---
+++ day_one/phase_seven.rb
@@ -0,0 +1,22 @@
+=begin
+# Classes
+
+Everything in Ruby is some kind of object.
+Classes explain objects, how a certain object works.
+
+For example:
+You made a few Blurb objects and these objects are
+explained in the Blurb class.
+
+# Accessors
+
+Accessors are variables attached to an object which
+can be used 'outside' to the object.
+(blurb2.time = Time.now)
+
+# Object Variables
+
+Object variables are the same variables you're
+using for accessors, but 'inside' the object.
+(@time = Time.now)
+=end
