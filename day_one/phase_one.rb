b16a1c910e27d1e3ba751d6964a248ae3acbf212
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2023-07-01 13:57:39 +0700

    Create `phase_one.rb`

---
+++ day_one/phase_one.rb
@@ -0,0 +1,11 @@
+# Phase 1 Already
+#puts 40.to_s.reverse
+
+# to_s converts to strings
+# to_i converts to integers
+# to_a converts to arrays
+
+fruits = ['Apple', 'Watermelon', 'Salak']
+
+#puts fruits.sort!
+#puts "I want to eat an #{fruits[0]}"
