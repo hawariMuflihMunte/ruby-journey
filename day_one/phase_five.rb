35804bdc068ad95d9373f82d2b8f923c2ee64e8b
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2023-07-01 15:41:17 +0700

    If only I knew how to make a decision

--- day_one/phase_five.rb
+++ day_one/phase_five.rb
@@ -201,3 +201,8 @@ def print_plays(hash_val, year_from, year_to)
 end
 
 #print_plays(get_shakey, 1600, 1605)
+
+# If only I knew how to make a decision
+if 1 < 2
+  puts "It is true: 1 is less than 2"
+end
