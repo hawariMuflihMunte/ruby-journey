1b79b59fa6c42f471a65ab15556c5b3eebb202ca
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2023-07-01 16:05:30 +0700

    Give explanation comment in multiline

--- day_one/phase_six.rb
+++ day_one/phase_six.rb
@@ -1,3 +1,24 @@
 =begin
-This is a multiline comment.
+
+You're a level six Ruby cleric. I mean what a great
+job you've done. Let's review:
+
+# Data
+
+You loaded some data off the internet, traversed a
+data structure and selected values.
+
+# Iterating
+
+You iterated all elements of a hash you chainged
+some more methods.
+
+# Pretty Printing
+
+And if that wasn't enough, you formatted and
+printed some values in a way that is easy to read
+for humans. In fact 'you made a real program'.
+
+# IF-ELSIF-ELSE statement
+
 =end
