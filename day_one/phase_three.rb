96090308579abfa14cda15ccb94f2e6388898794
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2023-07-01 14:04:37 +0700

    Initiate filler for hash

--- day_one/phase_three.rb
+++ day_one/phase_three.rb
@@ -1,1 +1,11 @@
-books = {}
+books = {
+  "Moby Dick" => :splendid,
+  "To Kill a Mockingbird" => :timeless
+}
+
+# :<value> is a symbol used as the value
+# associated with the key (left side value)
+
+books["Gravity Rainbow"] = :splendid
+
+puts books
