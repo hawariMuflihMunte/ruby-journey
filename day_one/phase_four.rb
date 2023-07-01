8dc6384b2cc3449d08f93813f90d9c2684bec5cf
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2023-07-01 14:41:47 +0700

    To(be) or not to(be)

---
+++ day_one/phase_four.rb
@@ -0,0 +1,9 @@
+# To(be) or not to(be)
+
+# Actually, you can omit parentheses when
+# calling a function/method. Ruby doesn't mind it though.
+
+# For example:
+# puts "Hello"
+# puts("Hello")
+# These examples both are fine and works.
