0548b8b7a055f19b3ce17b204fdd5801c58285f6
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2023-07-02 11:04:41 +0700

    Create `until_loop.rb`

---
+++ day_2/until_loop.rb
@@ -0,0 +1,10 @@
+=begin
+Similar to the while loop, but it continues
+execution until a specified condition becomes true.
+=end
+
+counter = 0
+until counter >= 5 do
+  puts counter
+  counter += 1
+end
