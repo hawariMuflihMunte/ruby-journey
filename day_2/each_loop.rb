7e6703473afb793cdfbd98b6f03b9fae992ad0b1
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2023-07-02 11:09:50 +0700

    Create `each_loop.rb` + Iterate through an array

---
+++ day_2/each_loop.rb
@@ -0,0 +1,11 @@
+=begin
+Commonly used to iterate over elements in
+a collection, such as an array or a hash.
+It applies a block of code to each element.
+=end
+
+# Iterating over an array
+fruits = ["apple", "banana", "orange"]
+fruits.each do |fruit|
+  puts fruit
+end
