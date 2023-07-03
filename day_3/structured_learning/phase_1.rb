2cc4a67663739aa8f744613142495e7839c9b731
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2023-07-03 13:07:56 +0700

    `elsif` statements

---
+++ day_3/structured_learning/phase_1.rb
@@ -0,0 +1,14 @@
+# elsif Statements in Ruby
+
+print "enter a number: "
+num = gets.chomp.to_i
+
+if num == 5
+  print "number is 5"
+elsif num == 10
+  print "number is 10"
+elsif num == 11
+  print "number is 11"
+else
+  print "number is something other than 5, 10, 11"
+end
