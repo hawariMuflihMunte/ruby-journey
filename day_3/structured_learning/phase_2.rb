99a58e231b310c4806e9020ea283ddfb17d14591
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2023-07-03 13:10:55 +0700

    `not` operator

---
+++ day_3/structured_learning/phase_2.rb
@@ -0,0 +1,11 @@
+# `not` operator
+
+=begin
+The ! (not) operator in Ruby flips a boolean value.
+If a value is `true` then applying ! to the value
+changes it to `false` and vice versa.
+=end
+
+data = true
+
+puts !data
