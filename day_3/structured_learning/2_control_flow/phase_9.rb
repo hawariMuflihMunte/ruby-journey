75fabbebe36682f0a296eccbde6c13bf85e5cc1e
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2023-07-03 13:33:59 +0700

    Range Ruby Inclusive

---
+++ day_3/structured_learning/2_control_flow/phase_9.rb
@@ -0,0 +1,13 @@
+# Range Ruby
+
+=begin
+A sequence of integers can be demonstrated by a range.
+The range can be divided into an inclusive range where
+the last integer in the sequence is included and an
+exclusive range where the last integer is excluded.
+=end
+
+# Inclusive
+(3..5).each do |i|
+  puts i
+end
