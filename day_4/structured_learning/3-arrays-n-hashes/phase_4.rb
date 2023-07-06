372b2a20bc3512bae65a91ff5c1cacf545a63216
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2023-07-07 00:29:59 +0700

    Hash bracket notation adding pairs

---
+++ day_4/structured_learning/3-arrays-n-hashes/phase_4.rb
@@ -0,0 +1,11 @@
+# Bracket notation applies to any hash,
+# regardless of how it was initialized
+
+teammates = Hash.new
+teammates['forward'] = 'Messi'
+
+salary = {
+  'starting' => 50000
+}
+
+salary['mid-level'] = 80000
