fdc61cd1e4103369cf35e8b432a99c3343bb52b8
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2023-07-07 00:27:05 +0700

    Hash.new

---
+++ phase_3.rb
@@ -0,0 +1,10 @@
+# Creating a hash through literal notation
+lunch = {
+  "protein" => "chicken",
+  "greens" => "lettuce",
+  "organic?" => true
+}
+
+# Creating a hash through Hash.new
+lunch = Hash.new
+puts lunch
