219660517017b11a99a82baeab316da0238f7075
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2023-07-14 19:43:59 +0700

    Case statement

---
+++ day_5/topic_5.rb
@@ -0,0 +1,12 @@
+# Case statement
+
+tv_show = "Bob's Burgers"
+
+case tv_show
+  when "Archer"
+    puts "I don't like the voice of Archer."
+  when "Bob's Burgers"
+    puts "I love the voice of Bob Belcher."
+  else
+    puts "I don't know who voices this cartoon"
+end
