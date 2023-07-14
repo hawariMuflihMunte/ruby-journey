13a574bd04159cb192848535f351a0d9acd4ab6e
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2023-07-14 19:34:26 +0700

    Ruby Symbols

---
+++ day_5/topic_3.rb
@@ -0,0 +1,15 @@
+# Symbols
+
+=begin
+`symbols` are immutable names primarily used as
+hash keys or referencing methods names.
+=end
+
+my_bologna = {
+  :first_name => 'Oscar',
+  :second_name => 'Meyer',
+  :slices => 12
+}
+
+puts my_bologna
+puts my_bologna[:first_name]
