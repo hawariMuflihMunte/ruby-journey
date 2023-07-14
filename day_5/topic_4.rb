ef5bb3639d86718a38b6bceb11fec2e11f063aea
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2023-07-14 19:37:56 +0700

    Hashed, Symbols, Values

---
+++ day_5/topic_4.rb
@@ -0,0 +1,21 @@
+# Ruby Hashed, Symbols, Values
+
+# =>
+my_progress = {
+  :program => 'Buatusaha',
+  :language => 'Ruby',
+  :enthusiastic? => true
+}
+
+# :
+my_alt_progress = {
+  program: 'Buatusaha',
+  language: 'Ruby',
+  enthusiastic?: true
+}
+
+# Both works fine
+
+puts my_progress
+
+puts my_alt_progress
