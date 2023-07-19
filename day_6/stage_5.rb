f8642f38dce7c451e3e843691d1a7e41b3f0100d
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2023-07-19 16:38:18 +0700

    Explainer on `Conditional Assignment Operator`

--- day_6/stage_5.rb
+++ day_6/stage_5.rb
@@ -5,5 +5,16 @@ boyfriend = nil
 boyfriend ||= 'Jimmy Jr.'
 
 boyfriend ||= 'Josh'
+boyfriend ||= 'Krem'
 
 puts boyfriend
+
+=begin
+
+Since the original value of boyfriend is set to nil
+which is nothing, Ruby assigns it a value of 'Jimmy Jr.'
+on the following line. Once boyfriend holds this real
+value, another reassignment is overlooked by Ruby and
+the previous value holds
+
+=end
