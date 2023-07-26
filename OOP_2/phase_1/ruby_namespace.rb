fc6a350051ad22486722a1cc9d0295aadbb73124
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2023-07-26 20:47:19 +0700

    Ruby namespace

---
+++ OOP_2/phase_1/ruby_namespace.rb
@@ -0,0 +1,12 @@
+# Ruby namespace
+
+=begin
+The term `namespace` refers to a module the contains a group of
+related objects.
+=end
+
+puts Math::PI
+
+# Ruby is targetting the PI constant from the Math module using
+# the scope resolution operator, (::), and printing its value
+# to the console.
