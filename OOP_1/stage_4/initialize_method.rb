28191836a86fff770006120c62d3e6cd1a496e70
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2023-07-26 20:23:23 +0700

    Ruby initialize method

---
+++ OOP_1/stage_4/initialize_method.rb
@@ -0,0 +1,10 @@
+# Ruby initialize method
+
+class Person
+  def initialize
+    # this code runs when a new instance is created
+  end
+end
+
+# Every time Person.new is called, the `initialize` method
+# of the Person class is called.
