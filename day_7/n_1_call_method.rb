90a6d1ee91f933fd987bf66a15924fc65566529b
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2023-07-26 18:15:42 +0700

    Ruby `.call` method

---
+++ day_7/n_1_call_method.rb
@@ -0,0 +1,26 @@
+# Ruby .call Method
+
+=begin
+a "proc" and a "lambda" can be called directly using
+the `.call` method.
+=end
+
+proc_test = Proc.new { puts "I am the proc method! "}
+lambda_test = lambda { puts "I am the lambda method! "}
+
+proc_test.call # => I am the proc method!
+lambda_test.call # => I am the lambda method!
+
+=begin
+The following code would result in
+
+"I am the proc method!"
+
+and
+
+"I am the lambda method!"
+
+printed to the console respectively,
+once the proc, proc_test, and
+the lambda, lambda_test, are called.
+=end
